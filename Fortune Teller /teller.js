c475237d74d27674958f4a495faccdb7b1a03ec3
Ilya Nikiporov <ilya.nikiporov@shopify.com>
2017-08-31 12:21:40 -0400

    Stuff

--- Fortune Teller /teller.js
+++ Fortune Teller /teller.js
@@ -1,9 +1,19 @@
+let dC = []
 function getFortune(){
-  let fortune = ["You will die today","You will find luck in a bar","You will be sad unless you eat less tacos"];
+  let fortune = ["Your high-minded principles spell success.","You will soon find what you seek","You will be sad unless you eat less tacos"];
     let rand = Math.round(Math.round(Math.random()*10)/4);
 if (fortune[rand] == undefined){ // Error handling in case the index called is outside of the # of fortunes we have
       rand = 0
 }
+dC.push(rand);  
+console.log("Potentially duplicate input value is " + rand);
+console.log(dC);
+console.log(dC[dC.length-2]);
+if (rand == dC[dC.length-2]){
+  console.log("DUPLICATE!");
+  rand+=1
+}
+console.log("Non duplicate value: " + rand)
 document.getElementById("test").innerHTML = fortune[rand]; // Write the fortune to the page
 }
 function load(){ // Initializes the button listener on body load
